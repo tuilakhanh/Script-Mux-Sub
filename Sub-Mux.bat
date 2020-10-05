b005737ed458e18f072e4accd8fca6bfa0852bfd
Khanh Le <bacdau852@gmail.com>
2020-10-05 19:31:11 +0700

    4.0

--- Sub-Mux.bat
+++ Sub-Mux.bat
@@ -1,18 +1,16 @@
 @echo off
 echo ----------------------------------------------------------------------
 echo   Auto-mux script for softsub
-echo   version 3.0 by tuilakhanh
-::ver2
-::support ?tf font extension
-::donot process with the second raw
+echo   version 4.0 by tuilakhanh
 echo ----------------------------------------------------------------------
 echo.
-set videofileformat=m2ts mp4 mkv avi ts wmv flv flv mpeg mpeg 3gp webm vob ogv ogg drc gif gifv mng mov qt yuv rm rmvb asf m4p m4v mpg mp2 mpe mpv mpg m2v m4v svi 3g2 mxf roq nsv flv f4v f4p f4a f4b olshiro ket korin
-::set videofileformat=mkv
-::set subtitlefileformat=ass srt ssa pgs
-set subtitlefileformat=srt ass ssa pgs
+set videofileformat=m2ts mp4 mkv avi ts wmv flv flv mpeg mpeg 3gp webm vob ogv ogg drc gif gifv mng mov qt yuv rm rmvb asf m4p m4v mpg mp2 mpe mpv mpg m2v m4v svi 3g2 mxf roq nsv flv f4v f4p f4a f4b
 set ISOlanguage=vie
 set subtitledefault=0:yes
+
+:sub
+set /P g=Choose subtitles format:
+set subtitlefileformat=%g%
 
 
 Setlocal EnableDelayedExpansion
@@ -30,70 +28,66 @@ for /l %%A IN (0,1,!taplimit!) do (
  if %%A LSS 10 set count=0%%A
  
  ::With each sub
- for %%E IN (%subtitlefileformat%) do (
+ for %%B IN ("*!count!*.%subtitlefileformat%") do (
   if not "!issuccess!"=="true" (
-  for %%B IN ("*!count!*.%%E") do (
+   ::With each video extension
+   for %%Z IN (%videofileformat%) do (
     if not "!issuccess!"=="true" (
-     ::With each video extension
-     for %%Z IN (%videofileformat%) do (
+     ::Search video
+     for %%C IN ("*!count!*.%%Z") do (
       if not "!issuccess!"=="true" (
-       ::Search video
-       for %%C IN ("*!count!*.%%Z") do (
-        if not "!issuccess!"=="true" (
-         
-         ::Print sub, video name
-         echo  -----------------------------------------------
-         echo  Episode !count!
-         echo   Output: %%~nB.mkv
-         echo   Raw: %%~C
-         echo   Sub: %%~B
-         
-         ::Collect Font, use all .tff
-         set fontstring=
-         for %%E IN (".\Font\*.?tf") do (
-          set fontstring=!fontstring! --attachment-name "%%~nxE" --attachment-mime-type application/x-truetype-font --attach-file "%%E"
-          echo     Font: %%~nxE
-         )
-         for %%E IN (".\*.?tf") do (
-          set fontstring=!fontstring! --attachment-name "%%~nxE" --attachment-mime-type application/x-truetype-font --attach-file "%%E"
-          echo     Font: %%~nxE
-         )
-         
-         ::Collect Chapter, just use one chapter
-         set chapterstring=
-         set chapterfile=
-         for %%G IN ("*!count!*.txt") do (
-          set chapterstring=--chapter-language vie --chapters "%%~G"
-          set chapterfile=%%G
-          echo     Chapter: %%~G
-         )
-         for %%G IN ("*!count!*.xml") do (
-          set chapterstring=--chapter-language vie --chapters "%%~G"
-          set chapterfile=%%G
-          echo     Chapter: %%~G
-         )
-         
-         ::execute muxing
-         echo.
-         "!mkvmergepath!" -o "%%~nB.mkv" -S "%%~C" --forced-track "%subtitledefault%" --default-track "%subtitledefault%" --language "0:%ISOlanguage%" "%%~B" !fontstring! !chapterstring!
-         echo.
-         
-         ::Move to old folder
-         if not exist ".\old" mkdir ".\old"
-         move /y "%%~nxB" ".\old\%%~nxB"
-         move /y "%%~nxC" ".\old\%%~nxC"
-         if not "!chapterfile!"=="" ( move /y ".\!chapterfile!" ".\old\!chapterfile!" )
-
-         ::Success first video
-         set issuccess=true
-        )
+       
+       ::Print sub, video name
+       echo  -----------------------------------------------
+       echo  Episode !count!
+       echo   Output: %%~nB.mkv
+       echo   Raw: %%~C
+       echo   Sub: %%~B
+       
+       ::Collect Font, use all .tff
+       set fontstring=
+       for %%E IN (".\Font\*.?tf") do (
+        set fontstring=!fontstring! --attachment-name "%%~nxE" --attachment-mime-type application/x-truetype-font --attach-file "%%E"
+        echo     Font: %%~nxE
        )
+       for %%E IN (".\*.?tf") do (
+        set fontstring=!fontstring! --attachment-name "%%~nxE" --attachment-mime-type application/x-truetype-font --attach-file "%%E"
+        echo     Font: %%~nxE
+       )
+       
+       ::Collect Chapter, just use one chapter
+       set chapterstring=
+       set chapterfile=
+       for %%G IN ("*!count!*.txt") do (
+        set chapterstring=--chapter-language vie --chapters "%%~G"
+        set chapterfile=%%G
+        echo     Chapter: %%~G
+       )
+       for %%G IN ("*!count!*.xml") do (
+        set chapterstring=--chapter-language vie --chapters "%%~G"
+        set chapterfile=%%G
+        echo     Chapter: %%~G
+       )
+       
+       ::execute muxing
+       echo.
+       "!mkvmergepath!" -o "%%~nB.mkv" -S "%%~C" --forced-track "%subtitledefault%" --default-track "%subtitledefault%" --language "0:%ISOlanguage%" "%%~B" !fontstring! !chapterstring!
+       echo.
+       
+       ::Move to old folder
+       if not exist ".\old" mkdir ".\old"
+       move /y "%%~nxB" ".\old\%%~nxB"
+       move /y "%%~nxC" ".\old\%%~nxC"
+       if not "!chapterfile!"=="" ( move /y ".\!chapterfile!" ".\old\!chapterfile!" )
+       
+       ::Success first video
+       set issuccess=true
       )
      )
     )
    )
   )
- ) 
+ )
 )
 
 :Delete
@@ -101,8 +95,6 @@ for /l %%A IN (0,1,!taplimit!) do (
 ::Delete old file
 set /P c=Do you want to delete old file?[Y/N]?
 if /I "%c%" EQU "Y" @RD /S /Q ".\old"
-
-
 
 @echo off
 :CONTINUE3
